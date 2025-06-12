94c7b69bbe0249c9f1c795c5aa658cb5a82f0faa
Jos3PM <just76111@hotmail.com>
2025-06-11 21:04:22 -0500

    Add files via upload

---
+++ Ajedres/src/main/java/upeu/edu/pe/ajedres/AjedresApplication.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.ajedres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjedresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjedresApplication.class, args);
+	}
+
+}
